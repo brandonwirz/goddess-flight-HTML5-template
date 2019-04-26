78e4882ae9f27ef2e708d542c7ade05244fba264
Brandon Wirz <bwirz75@gmail.com>
2019-04-26 17:22:59 -0600

    home page created

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
 // jQuery script
-$("#top-menu ul li a, #nav-menu ul li a").click(function(e){
-    e.preventDefault();
-    console.log("working>>>")
-    $(".toggle").hide();
-    var showContent = $(this).attr('href');
-    $(showContent).show();
-});
+// $( document).ready(function() {
+  // $('#top-menu ul li a, #nav-menu ul li a','#left-nav-menu ul li a').on('click', function(e){
+  // $('#top-menu ul li a, #nav-menu ul li a, #left-nav-menu ul li a').click(function(e){
+  //     e.preventDefault();
+  //     console.log("working>>>")
+  //     $(".toggle").hide();
+  //     var showContent = $(this).attr('href');
+  //     $(showContent).show('id');
+  //     // $(showContent).show('url');
+  // });
+  // $("#[ahref*=#]").click(function(e){
+  //     e.preventDefault();
+  //     console.log("working>>>")
+  //     $(".toggle").hide();
+  //     var showContent = $(this).attr('href');
+  //     $(showContent).show('id');
+  //     // $(showContent).show('url');
+  // });
+
+  // $("#top-menu ul li a, #nav-menu ul li a").click(function(e){
+  //     e.preventDefault();
+  //     console.log("working>>>")
+  //     $(".toggle").hide();
+  //     var showContent = $(this).attr('href');
+  //     $(showContent).show('id');
+  // });
+
+ // $("#[ahref*=#]").click(function(e){
+ //     e.preventDefault();
+ //     console.log("working>>>")
+ //     $(".toggle").hide();
+ //     var showContent2 = $(this).attr('href');
+ //     $(showContent2).show('id');
+ // });
