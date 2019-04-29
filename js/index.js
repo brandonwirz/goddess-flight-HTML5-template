a2a8e8c91e2309d217ce90a9da15b1428e512fb5
Brandon Wirz <bwirz75@gmail.com>
2019-04-29 01:36:37 -0600

    implemented hamburger menu, need to make final adjustments

--- js/index.js
+++ js/index.js
@@ -1,35 +1,12 @@
-// jQuery script
-// $( document).ready(function() {
-  // $('#top-menu ul li a, #nav-menu ul li a','#left-nav-menu ul li a').on('click', function(e){
-  // $('#top-menu ul li a, #nav-menu ul li a, #left-nav-menu ul li a').click(function(e){
-  //     e.preventDefault();
-  //     console.log("working>>>")
-  //     $(".toggle").hide();
-  //     var showContent = $(this).attr('href');
-  //     $(showContent).show('id');
-  //     // $(showContent).show('url');
-  // });
-  // $("#[ahref*=#]").click(function(e){
-  //     e.preventDefault();
-  //     console.log("working>>>")
-  //     $(".toggle").hide();
-  //     var showContent = $(this).attr('href');
-  //     $(showContent).show('id');
-  //     // $(showContent).show('url');
-  // });
-
-  // $("#top-menu ul li a, #nav-menu ul li a").click(function(e){
-  //     e.preventDefault();
-  //     console.log("working>>>")
-  //     $(".toggle").hide();
-  //     var showContent = $(this).attr('href');
-  //     $(showContent).show('id');
-  // });
-
- // $("#[ahref*=#]").click(function(e){
- //     e.preventDefault();
- //     console.log("working>>>")
- //     $(".toggle").hide();
- //     var showContent2 = $(this).attr('href');
- //     $(showContent2).show('id');
- // });
+$(document).ready(function() {
+	var n = '#nav'
+  var n2 = '#nav2'
+	$('#nav-menu').click(function() {
+		if ($(n).hasClass('nav-open') && $(n2).hasClass('nav-open')) {
+			($(n).removeClass('nav-open') && $(n2).removeClass('nav-open')) ;
+		}
+		else {
+			($(n).addClass('nav-open') && $(n2).addClass('nav-open'));
+		}
+	});
+});
