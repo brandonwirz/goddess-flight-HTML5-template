42e377f446a25bb58a8c618de29ac0e32a4d7c25
Brandon Wirz <bwirz75@gmail.com>
2019-04-29 17:04:51 -0600

    completed HTML5 responsive template

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var n = '#nav'
-  var n2 = '#nav2'
+	const n = '#nav'
+  const n2 = '#nav2'
 	$('#nav-menu').click(function() {
 		if ($(n).hasClass('nav-open') && $(n2).hasClass('nav-open')) {
 			($(n).removeClass('nav-open') && $(n2).removeClass('nav-open')) ;
