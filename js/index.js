12c5297dae057c62a5a90423e7821fa53bb517e5
Brandon Wirz <bwirz75@gmail.com>
2019-04-30 00:56:45 -0600

    ready for deployment

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	const n = '#nav'
   const n2 = '#nav2'
-	$('#nav-menu').click(function() {
+	$('#nav-menu-top').click(function() {
 		if ($(n).hasClass('nav-open') && $(n2).hasClass('nav-open')) {
 			($(n).removeClass('nav-open') && $(n2).removeClass('nav-open')) ;
 		}
